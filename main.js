282b53cb49e6e6944d315c4e2096764656adcc96
Puripant <puripant@gmail.com>
2018-01-23 15:04:41 +0700

    ignore special keys (except backspace) add "edited" class for edited text

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ var wordcut = require("wordcut");
 wordcut.init();
 var words = wordcut.cut(text).split("|");
 
-text = "<span>" + words.join("</span><span>") + "</span>";
-text = text.replace(new RegExp("<span> </span>", "ig"), " ");
-text = text.replace(new RegExp("<span>[\r\n\f]</span>", "ig"), "<br>");
-text = text.replace(new RegExp("<span>[\r\n\f]+</span>", "ig"), "<br><br>");
-lyric.innerHTML = text;
+text = "<span class=' '>" + words.join("</span><span class=' '>") + "</span>";
+text = text.replace(new RegExp("<span class=' '> </span>", "ig"), " ");
+text = text.replace(new RegExp("<span class=' '>[\r\n\f]</span>", "ig"), "<br>");
+text = text.replace(new RegExp("<span class=' '>[\r\n\f]+</span>", "ig"), "<br><br>"); //NOTE 'edited' class will be added here
+lyric.innerHTML = text; //NOTE 'highlight' class will be added here
 
 // lyric.addEventListener("mouseover", function(event) {
 //   targetText = event.target.innerText;
@@ -17,32 +17,35 @@ lyric.innerHTML = text;
 //   lyric.innerHTML = text.replace(regex, "<span class='editable'>$1</span>");
 // }, false);
 
+var targetSpan;
 var targetText;
 var justClicked = false;
 lyric.addEventListener("click", function(event) {
-  targetText = event.target.innerText;
-  var regex = new RegExp("<span>(" + targetText + ")</span>", "ig");
-  lyric.innerHTML = text.replace(regex, "<span class='highlight'>$1</span>");
-  // lyric.innerHTML = lyric.innerHTML.replace(/editable/ig, "highlight");
+  targetSpan = event.target;
+  targetText = targetSpan.innerText;
+  var regex = new RegExp("<span class='(.+?)'>(" + targetText + ")</span>", "ig");
+  lyric.innerHTML = text.replace(regex, "<span class='$1 highlight'>$2</span>");
 
   justClicked = true;
 }, false);
 
 document.addEventListener("keyup", function(event) {
-  //TODO check special keys
-  if (lyric.querySelectorAll(".highlight")) {
-    var regex = new RegExp(targetText, "ig");
-    if (justClicked) { //text replacement
-      targetText = event.key;
-    } else { //append key
-      targetText += event.key;
+  if (lyric.querySelectorAll(".highlight") && (event.key == "Backspace" || event.key.match(/^.$/i))) {
+    var regex = new RegExp("<span class='(.+?)'>" + targetText + "</span>", "ig");
+
+    if (event.key == "Backspace") {
+      targetText = targetText.substring(0, targetText.length-1);
+    } else {
+      if (justClicked) { //text replacement
+        targetText = event.key;
+      } else { //append key
+        targetText += event.key;
+      }
     }
-    text = text.replace(regex, targetText);
-    // lyric.innerHTML = text;
-    lyric.innerHTML = lyric.innerHTML.replace(regex, targetText);
 
-    //TODO somehow highlight the edits
-
+    text = text.replace(regex, "<span class='$1 edited'>" + targetText + "</span>");
+    lyric.innerHTML = text;
+    
     justClicked = false;
   }
 });
